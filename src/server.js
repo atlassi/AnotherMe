705a1c2a415834d177159bd703d4d25cc27c103e
MumtazAkhtar <mumtazmohammed08@gmail.com>
2017-11-23 17:55:32 +0100

    added bcrypt

--- src/server.js
+++ src/server.js
@@ -2,11 +2,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
+const bcrypt = require('bcrypt');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 //<------configuring dependencies------->
-const sequelize = new Sequelize('another-me', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+
+const app = express();
+
+
+const sequelize = new Sequelize('anotherme', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
     host: 'localhost',
     dialect: 'postgres',
     storage: './session.postgres'
@@ -29,27 +34,83 @@ app.use(session({
     resave: false
 }))
 
-//<----Defining models-------->
+//<----------Defining models------------>
 const User = sequelize.define('users',{
 	name: Sequelize.STRING,
 	email: Sequelize.STRING,
 	password: Sequelize.STRING,
 	phone: Sequelize.INTEGER,
-	city: Sequelize.STRING
+	city: Sequelize.STRING,
 	address: Sequelize.STRING,
-	availability: Sequelize.STRING
-	service-provider: Sequelize.BOOLEAN,
+	availability: Sequelize.STRING,
+	serviceProvider: Sequelize.BOOLEAN
 },{ timestamps: false
 })
 
 const Role = sequelize.define('roles',{
 	service: Sequelize.STRING,
-	price: Sequelize.STRING,
+	price: Sequelize.STRING
 },{timestamps: false
 })
 
-//<<-----------ROUTES------------->>
+//<----defining relation between tables---->
+User.hasMany(Role)
 
+//<<-----------------ROUTES-------------------->>
+
+//<----default page------->
 app.get('/', function(req,res){
+	let user = req.session.user
 	res.render("index")
+})
+
+//<----search page(get)------>
+app.get('search',function(req,res){
+	res.render("search")
+})
+
+//<-----SIGNUP(post)------>
+app.post('/signup',function(req,res){
+
+let inputname = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password;
+
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            User.create({
+                name: inputname,
+                email: email,
+                password: hash
+            }).then((user) => {
+                req.session.user = user;
+                res.render('index.pug')
+            })
+
+        });
+    }) 
+
+})
+
+//<---------LOGIN(post)--------->
+app.post('/login',function(req,res){
+	res.redirect('/')
+})
+
+//<----Search for service providers(post)-->
+app.post('/search',function(req,res){
+	res.render('index')
+})
+
+//<-------ADD DETAILS(post)-------->
+app.post('/addDetals',function(req,res){
+	res.render('index')
+})
+
+
+
+sequelize.sync()
+//<--------SERVER PORT----------->
+app.listen(4000, function() {
+    console.log("app is listening at port 4000")
 })
